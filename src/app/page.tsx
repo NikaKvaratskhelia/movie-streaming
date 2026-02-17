63b1fb6a74826587eef62b953940413825fd70d5
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-02-17 19:18:59 +0400

    chore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
-import MovieHeroSection from "@/src/components/home/movie-hero-section";
 import RecentlyUpdated from "@/src/components/home/RecentlyUpdated";
-import MovieSections from "@/src/components/home/movie-sections";
+import MovieSections from "@/src/components/home/MovieSections";
 
 export default function Home() {
-
   return (
     <>
-    
-      <MovieHeroSection />
-    
       <RecentlyUpdated />
 
       <MovieSections />
     </>
   );
 }
-
-
-
