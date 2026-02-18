4a436d35fe7cdd39dbbad374877d2457f0f1f84a
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-02-19 02:45:39 +0400

    fix:loading

--- src/components/home/MovieSections.tsx
+++ src/components/home/MovieSections.tsx
@@ -3,8 +3,10 @@ import StyledMovieCard from "../shared/MovieCardRating";
 import LoadingSpinner from "../shared/LoadingSpinner";
 import ErrorComponent from "../shared/ErrorComponent";
 import NoData from "../shared/NoData";
+import ViewAllLink from "../shared/ViewAllLink";
 import { useMovieStore } from "../../store/useMovieStore";
 import { useEffect } from "react";
+import { useDataOnLoaded } from "../../hooks/useDataOnLoaded";
 
 export default function MovieSections() {
   const { movies, loading, error, fetchMovies } = useMovieStore();
@@ -13,15 +15,20 @@ export default function MovieSections() {
     fetchMovies();
   }, [fetchMovies]);
 
+  useDataOnLoaded({ data: movies, loading });
+
   if (loading) return <LoadingSpinner />;
   if (error) return <ErrorComponent error={error} />;
   if (movies.length === 0) return <NoData />;
 
   return (
     <section className="w-full">
-      <h2 className="ml-[12%] mt-20 mb-6 text-2xl font-semibold text-white">
-        Trending
-      </h2>
+      <div className="w-[75%] flex justify-between items-center ml-[12%] mr-[12%] mt-20 mb-6">
+        <h2 className="text-2xl font-semibold text-white">
+          Trending
+        </h2>
+        <ViewAllLink href="/trending" />
+      </div>
 
       <div className="flex justify-center">
         <div className="flex gap-8">
