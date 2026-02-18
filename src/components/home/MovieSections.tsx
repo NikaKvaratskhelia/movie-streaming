f0549288478316b6de1f9a8cddc7f642275cbfe0
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-02-18 20:48:41 +0400

    feet:add title

--- src/components/home/MovieSections.tsx
+++ src/components/home/MovieSections.tsx
@@ -1,6 +1,5 @@
 "use client";
 import StyledMovieCard from "../shared/MovieCardRating";
-import MovieCard from "../shared/MovieCard";
 import LoadingSpinner from "../shared/LoadingSpinner";
 import ErrorComponent from "../shared/ErrorComponent";
 import NoData from "../shared/NoData";
@@ -14,29 +13,23 @@ export default function MovieSections() {
     fetchMovies();
   }, [fetchMovies]);
 
-  if (loading) {
-    return <LoadingSpinner />;
-  }
-
-  if (error) {
-    return <ErrorComponent error={error} />;
-  }
-
-  if (movies.length === 0) {
-    return <NoData />;
-  }
+  if (loading) return <LoadingSpinner />;
+  if (error) return <ErrorComponent error={error} />;
+  if (movies.length === 0) return <NoData />;
 
   return (
-    <section>
-      <div className="w-full flex justify-center">
-        <div className="flex gap-[33px]">
+    <section className="w-full">
+      <h2 className="ml-[12%] mt-20 mb-6 text-2xl font-semibold text-white">
+        Trending
+      </h2>
+
+      <div className="flex justify-center">
+        <div className="flex gap-8">
           {movies.slice(0, 3).map((movie) => (
             <StyledMovieCard key={movie.id} movie={movie} />
           ))}
         </div>
       </div>
-
-
     </section>
   );
 }
