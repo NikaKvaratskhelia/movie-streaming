cd7155bbc84d3f46d466fb689f274c129b874de4
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-02-20 23:23:11 +0400

    fix:Imoved the burger menu to another file and imported it cleanly into the header code

---
+++ src/components/header/navigation.ts
@@ -0,0 +1,8 @@
+export const navItems = [
+  { href: "/", label: "Home" },
+  { href: "/movies", label: "Movies" },
+  { href: "/series", label: "Series" },
+  { href: "/producers", label: "Producers" },
+  { href: "/actors", label: "Actors" },
+  { href: "/country", label: "Country" },
+];
