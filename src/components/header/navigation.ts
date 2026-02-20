c28f6d1769588345f4f16ddce832dac686e83713
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-02-20 23:26:17 +0400

    delete

--- src/components/header/navigation.ts
+++
@@ -1,8 +0,0 @@
-export const navItems = [
-  { href: "/", label: "Home" },
-  { href: "/movies", label: "Movies" },
-  { href: "/series", label: "Series" },
-  { href: "/producers", label: "Producers" },
-  { href: "/actors", label: "Actors" },
-  { href: "/country", label: "Country" },
-];
