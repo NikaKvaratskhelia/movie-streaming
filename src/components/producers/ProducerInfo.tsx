81e2f728d76d9e11991b77a548092531600fb4f6
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-02-27 01:21:45 +0400

    feat: add back button in producer info card

--- src/components/producers/ProducerInfo.tsx
+++ src/components/producers/ProducerInfo.tsx
@@ -1,4 +1,5 @@
 import { Movie } from "@/generated/prisma/browser";
+import BackButton from "../shared/BackButton";
 
 interface ProducerInfoProps {
   producer: {
@@ -13,30 +14,33 @@ interface ProducerInfoProps {
 
 export default function ProducerInfo({ producer }: ProducerInfoProps) {
   return (
-    <section className="relative mb-12 overflow-hidden rounded-3xl bg-linear-to-br from-zinc-900 via-black to-zinc-950 border border-zinc-800 p-10">
-      <div className="pointer-events-none absolute -top-32 -left-32 h-96 w-96 rounded-full bg-red-600/10 blur-3xl" />
+    <div>
+      <BackButton />
+      <section className="relative mb-12 overflow-hidden rounded-3xl bg-linear-to-br from-zinc-900 via-black to-zinc-950 border border-zinc-800 p-10">
+        <div className="pointer-events-none absolute -top-32 -left-32 h-96 w-96 rounded-full bg-red-600/10 blur-3xl" />
 
-      <div className="relative z-10">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white">
-          {producer.fullName}
-        </h1>
+        <div className="relative z-10">
+          <h1 className="text-5xl font-extrabold tracking-tight text-white">
+            {producer.fullName}
+          </h1>
 
-        <div className="mt-6 h-px w-full bg-linear-to-r from-transparent via-zinc-700 to-transparent" />
+          <div className="mt-6 h-px w-full bg-linear-to-r from-transparent via-zinc-700 to-transparent" />
 
-        <div className="mt-10 grid grid-cols-2 md:grid-cols-4 gap-8">
-          <InfoBlock label="Nationality" value={producer.nationality} />
+          <div className="mt-10 grid grid-cols-2 md:grid-cols-4 gap-8">
+            <InfoBlock label="Nationality" value={producer.nationality} />
 
-          <InfoBlock
-            label="Born"
-            value={new Date(producer.dateOfBirth).toLocaleDateString()}
-          />
+            <InfoBlock
+              label="Born"
+              value={new Date(producer.dateOfBirth).toLocaleDateString()}
+            />
 
-          <InfoBlock label="Debut" value={producer.debutYear} />
+            <InfoBlock label="Debut" value={producer.debutYear} />
 
-          <InfoBlock label="Movies" value={producer.movies.length} />
+            <InfoBlock label="Movies" value={producer.movies.length} />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
 
