7113b69ad935c805613dedcb26e26fe337c21bc2
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2026-03-01 20:08:01 +0400

    refactor: actors page

--- src/app/(site)/actors/page.tsx
+++ src/app/(site)/actors/page.tsx
@@ -1,15 +1,5 @@
-"use client";
-
-import { useActors } from "@/src/hooks/useActor";
 import ActorPageLayout from "@/src/components/actors/ActorPageLayout";
-import Loader from "@/src/components/ui/Loader";
 
 export default function ActorsPage() {
-  const { actors, isLoading } = useActors();
-  if (isLoading) {
-    return <Loader />;
-  }
-  return (
-    <ActorPageLayout actors={actors} isLoading={isLoading} />
-  );
+  return <ActorPageLayout />;
 }
