6f97050645a90849376bde384f6560444cdf4ff7
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2026-03-01 19:52:54 +0400

    refactor: folder structure

---
+++ src/components/admin/Hero.tsx
@@ -0,0 +1,9 @@
+
+export default function Hero() {
+  return (
+    <div className="w-full flex flex-col items-start justify-center gap-4 px-8 py-12">
+        <h1 className="text-white">Welcome back, <span className="text-red-600">Admin</span></h1>
+        <p>Manage your streaming platform content</p>
+    </div>
+  )
+}
