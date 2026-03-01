6c978bf450c205c205e5f1eb8e4036ba391517df
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2026-03-02 02:15:35 +0400

    feat: latest info section

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -4,7 +4,7 @@ export default function SiteLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
       <Header />
-      <main className="pt-20">
+      <main className="pt-20 pb-20 bg-black">
         {children}
       </main>
     </>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col bg-black pb-20">
+      <body className="min-h-screen flex flex-col">
         <Providers>
           {children}
           <Loader />
