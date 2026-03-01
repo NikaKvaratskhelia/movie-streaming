4ed82f969f5685bd9c3414d1de2358d15d54e166
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2026-03-01 20:08:01 +0400

    refactor: actors page

--- src/components/actors/ActorPageLayout.tsx
+++ src/components/actors/ActorPageLayout.tsx
@@ -1,40 +1,11 @@
-"use client";
-
-import { useState, useEffect } from "react";
-import { Actor } from "@/generated/prisma/browser";
-
 import Actors from "./Actors";
 
-interface ActorPageLayoutProps {
-  actors: Actor[];
-  isLoading: boolean;
-}
-
-export default function ActorPageLayout({
-  actors,
-  isLoading,
-}: ActorPageLayoutProps) {
-  const [filteredActors, setFilteredActors] = useState<Actor[]>(actors);
-
-  useEffect(() => {
-    setFilteredActors(actors);
-  }, [actors]);
-
-  if (isLoading) {
-    return (
-      <div className="min-h-screen bg-black flex items-center justify-center">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-white"></div>
-      </div>
-    );
-  }
+export default function ActorPageLayout() {
   return (
-    <div className="min-h-screen bg-black">
-
-
-      <div className="w-full px-4 lg:px-[12%] pb-8 lg:pb-12 my-4 py-10">
-        <div className="w-full mx-auto">
-          <Actors actors={filteredActors} />
-        </div>
+    <div className="min-h-screen bg-black mt-10">
+      <h1 className="text-4xl font-bold mb-8 text-center text-white">Actors</h1>
+      <div className="w-full px-4 pb-8 lg:pb-12 my-4 py-10 mx-auto">
+        <Actors />
       </div>
     </div>
   );
