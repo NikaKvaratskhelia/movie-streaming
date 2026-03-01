4761d56f4f98b99c98b4def6ec14d598bd015c42
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-01 21:46:23 +0400

    fix:fixed bugs in the pages code and simplified the code

--- src/app/(site)/series/page.tsx
+++ src/app/(site)/series/page.tsx
@@ -1,16 +1,5 @@
-"use client";
-
-import { useSeries } from "@/src/hooks/useSeries";
 import SeriesPageLayout from "@/src/components/series/SeriesPageLayout";
 
 export default function SeriesPage() {
-  const { series, isLoading, error } = useSeries();
-
-  return (
-    <SeriesPageLayout 
-      series={series} 
-      isLoading={isLoading} 
-      error={error} 
-    />
-  );
+  return <SeriesPageLayout />;
 }
---
+++ src/components/producers/ProducerPageLayout.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useProducers } from "@/src/hooks/useProducers";
+import ProducerList from "./ProducerList";
+import { Loader } from "lucide-react";
+import ErrorDisplay from "./ErrorDisplay";
+
+export default function ProducerPageLayout() {
+  const { producers, error, isLoading } = useProducers();
+
+  if (isLoading) return <Loader />;
+
+  if (error) {
+    return <ErrorDisplay error={error.message} />;
+  }
+
+  if (!producers) return null;
+
+  return (
+    <div className="min-h-screen bg-black text-white pt-24 px-4">
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-4xl font-bold mb-8 text-center">Producers</h1>
+        <ProducerList producers={producers} />
+      </div>
+    </div>
+  );
+}
