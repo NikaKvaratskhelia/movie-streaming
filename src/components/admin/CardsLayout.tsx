626e4f855fc7e77572ad3191cdb575f4843f472a
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2026-03-01 19:12:17 +0400

    fix: deleting user

--- src/components/admin/CardsLayout.tsx
+++ src/components/admin/CardsLayout.tsx
@@ -3,31 +3,22 @@
 import { useMovies } from "@/src/hooks/useMovie";
 import { useSeries } from "@/src/hooks/useSeries";
 import AdminCard from "./AdminCard";
-import { useProducers } from "@/src/hooks/useProducers";
 import Loader from "../ui/Loader";
+import { useAdminUsers } from "@/src/hooks/useAdminUsers";
 
 export default function CardsLayout() {
   const { movies, isLoading: loading1 } = useMovies();
   const { series, isLoading: loading2 } = useSeries();
-  const { producers, isLoading: loading3 } = useProducers();
+  const { users, loading } = useAdminUsers();
 
-  const isLoading = loading1 || loading2 || loading3;
+  const isLoading = loading1 || loading2 || loading;
 
   if (isLoading) return <Loader />;
   return (
     <div className="flex justify-between gap-5 items-center max-w-300 mx-auto my-30">
-      <AdminCard
-        text="Movies"
-        count={movies.length.toString()}
-      />
-      <AdminCard
-        text="Series"
-        count={series.length.toString()}
-      />
-      <AdminCard
-        text="Users"
-        count={producers.length.toString()}
-      />
+      <AdminCard text="Movies" count={movies.length.toString()} />
+      <AdminCard text="Series" count={series.length.toString()} />
+      <AdminCard text="Users" count={users.length.toString()} />
     </div>
   );
 }
