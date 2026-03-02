108151fbefda10aba950a63ae3edd7611fe6ff9a
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-02 23:10:16 +0400

    fix:fixed  trending  section title responsive

--- src/components/home/MovieSections.tsx
+++ src/components/home/MovieSections.tsx
@@ -6,16 +6,16 @@ import { useMovies } from "@/src/hooks/useMovie";
 
 export default function MovieSections() {
   const { movies = [] } = useMovies();
+
   return (
-    <section>
-      <div className="w-full flex justify-between items-center mt-12 lg:mt-20 mb-6 px-8">
+    <section className="flex flex-col items-start">
+      <div className="w-full flex flex-col lg:flex-row justify-between gap-4 mt-12 lg:mt-20 mb-6 px-8">
         <h2 className="text-xl lg:text-2xl font-semibold text-white">
           Trending
         </h2>
         <ViewAllLink href="/trending" />
       </div>
-
-      <div className="flex justify-center">
+      <div className="w-full flex justify-center">
         <div className="flex flex-wrap gap-8 justify-center">
           {movies.slice(0, 3).map((movie) => (
             <MovieCardRating key={movie.id} movie={movie} />
